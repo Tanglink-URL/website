58971c39e57f290a547a1d9d666b7e872dd617eb
Cody Ko In A Box <codykoinabox@gmail.com>
2023-06-30 19:54:58 -0300

    Final URL dynamically updates to show shortURL + boilerplate

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 let randCharSet = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789"
 
-
+let shortURLInput = document.getElementById("shortURLInput")
+let tangButton = document.getElementById("tangButton")
+let finalURL = document.getElementById("finalURL")
 
 
 
 
 function longURLInputChange(){
-    console.log("Hello!")
-
-    let shortURLInput = document.getElementById("shortURLInput")
-    let tangButton = document.getElementById("tangButton")
 
     shortURLInput.style.display = "block"
     tangButton.style.display = "block"
+    finalURL.style.display = "block"
 
     shortURLInput.value = randString(5, randCharSet)
+    finalURL.innerText = "tanglink.onrender.com/" + shortURLInput.value
+}
+
+function shortURLInputChange(){
+    finalURL.innerText = "tanglink.onrender.com/" + shortURLInput.value
 }
 
 
